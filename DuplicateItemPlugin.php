6daf7e1e05b9100146d348c21971c1ae08aeea0f
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2019-12-23 20:44:35 +0100

    Update DuplicateItemPlugin.php
    Adds the sections relative to Duplicate button and links

--- DuplicateItemPlugin.php
+++ DuplicateItemPlugin.php
@@ -16,10 +16,15 @@ class DuplicateItemPlugin extends Omeka_Plugin_AbstractPlugin
       $request = Zend_Controller_Front::getInstance()->getRequest();
       $controller = $request->getControllerName();
       $action = $request->getActionName();
-      if ($controller == 'items' && $action == 'browse') {            
-        queue_js_file('duplicate');
+      if ($controller == 'items') {
+        if ($action == 'browse') {            
+            queue_js_file('duplicate');
+        } elseif ($action == 'show') {
+            queue_js_file('duplicate-button');
+        }
+      } elseif ($controller == 'index' && $action == 'index') {
+			queue_js_string("duplicate-indexlinks');
       }
-      
     }
 
 }
